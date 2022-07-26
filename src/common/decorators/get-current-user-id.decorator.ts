26e45d1ee37b3a08739e0210e8a08c52a9a1a7b9
Jorge Guberte <jorgeguberte@gmail.com>
2022-07-26 18:04:13 -0300

    dev snapshot

--- src/common/decorators/get-current-user-id.decorator.ts
+++ src/common/decorators/get-current-user-id.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
 export const GetCurrentUserId = createParamDecorator(
     (data: undefined, context: ExecutionContext):string=>{
-        const request = context.switchToHttp().getRequest()
+        const request = context.switchToHttp().getRequest();
         //if(!data) return request.user;
         return request.user['sub'];
     },
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const whitelist = ['http://localhost:3000','https://scoutiveui.herokuapp.com'];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+  });
+  
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3001);
 }
