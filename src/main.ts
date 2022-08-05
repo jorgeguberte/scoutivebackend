23c6dd2bb0459b341b14319171960280a843b95c
Jorge Guberte <jorgeguberte@gmail.com>
2022-08-05 08:51:17 -0300

    Added Procfile

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
   });
   
   app.useGlobalPipes(new ValidationPipe({transform: true}))
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
