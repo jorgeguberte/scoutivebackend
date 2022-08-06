fa9ee6ca7058faa50d6f3b93420c72e5e8ca8f08
Jorge Guberte <jorgeguberte@gmail.com>
2022-08-06 13:12:09 -0300

    Fixed vulnerability

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,18 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { AtGuard } from './common/guards';
-import { PrismaModule } from './prisma/prisma.module';
 import { PlayersModule } from './players/players.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [AuthModule, PrismaModule, PlayersModule],
-  providers:[
+  providers: [
     {
       provide: APP_GUARD,
-      useClass: AtGuard
+      useClass: AtGuard,
     },
-    PrismaService
-  ]
+    PrismaService,
+  ],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AtStrategy, RtStrategy } from './strategies';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports:[JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, AtStrategy, RtStrategy]
+  providers: [AuthService, AtStrategy, RtStrategy, PrismaService],
 })
 export class AuthModule {}
