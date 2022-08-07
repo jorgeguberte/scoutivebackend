ddbbe322b796ff1a186db1d9b2b240ee5a3a94ff
Jorge Guberte <jorgeguberte@gmail.com>
2022-08-06 23:00:05 -0300

    Annotated Auth Dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class AuthDto{
+    @ApiProperty({
+        type: String,
+        description: 'User email',
+      })
     @IsNotEmpty()
     @IsString()
     email: string;
 
+
+    @ApiProperty({
+        type: String,
+        description: 'User password',
+      })
     @IsNotEmpty()
     @IsString()
     password: string;
